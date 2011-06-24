c04328c75ab4136786f0a4367f4e7880e7003ce8
Adam Crownoble <adam.crownoble@biola.edu>
2011-06-24 14:36:15 -0700

    New Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseThinkingSphinxIssues::Application.config.secret_token = '22aecd57fc1b04bf099f071c871eafd661d54679b29ff61958c25bef104ec0f74e13999396e8b1d14dc9aabf854fdfd4aef67470422dcdd64d0b19bcee199018'
